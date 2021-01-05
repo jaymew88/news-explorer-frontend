2228eb823adb8ceb0aae5414004ff4971a4a7a98
Jayme Waye <jaymewaye@gmail.com>
2021-01-04 19:03:47 -0500

    header, nav, searchform mobile

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -6,7 +6,11 @@ function Navigation() {
 
   return (
     <nav className="navbar">
-      
+      <p className="navbar__logo">NewsExplorer</p>
+      <button 
+        type="button"
+        className="navbar__hamburger"
+      ></button>
 
     </nav>
   );
---
+++ src/components/SearchForm/SearchForm.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './SearchForm.css';
+
+function SearchForm() {
+
+  return (
+    <>
+      <form 
+      className="search-form" 
+      action="#" 
+      name="search-form" 
+    // onSubmit={props.onSubmit}
+      noValidate 
+    >
+      <input 
+        className="search-form__input" 
+        name="search-form"
+        type="text"
+        title="search-form"
+        placeholder="Search"
+        minLength={2}
+        maxLength={40}
+        required
+      />
+      <button className="search-form__submit-btn" type="submit">Search</button>
+    </form>
+  </>
+  );
+}
+
+export default SearchForm;
