c79aadc0705fa2fc4572b634a8e2195573753672
Jayme Waye <jaymewaye@gmail.com>
2021-01-05 17:46:10 -0500

    header css responsive

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,18 +1,35 @@
 import React from 'react';
 import './Navigation.css';
 
-function Navigation() {
+function Navigation(props) {
 
 
   return (
+   <> 
+     {/* Mobile View */}
     <nav className="navbar">
       <p className="navbar__logo">NewsExplorer</p>
-      <button 
-        type="button"
-        className="navbar__hamburger"
-      ></button>
-
+      <button className="navbar__mobile" />
+      
+      <ul className="navbar__links">
+        <li className="navbar__home">Home</li>
+        <li className="navbar__signin">Sign in</li>
+      </ul>
     </nav>
+
+    {/* Tablet/Desktop View 
+    <nav className="navbar">
+      <div className="navbar__mobile-panel">
+        <p className="navbar__logo">NewsExplorer</p>
+       
+      </div>
+      <ul className="navbar__links">
+        <li className="navbar__options">Home</li>
+        <li className="navbar__options">Sign in</li>
+      </ul>
+
+    </nav> */}
+   </> 
   );
 }
 
